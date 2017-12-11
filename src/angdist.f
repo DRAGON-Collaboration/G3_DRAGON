691292cd1c3ce6308b160a2277caf87b30cbdcc2
elzoido238 <dconnolly@triumf.ca>
2017-12-10 22:30:08 -0800

    correct dipole distribution

--- src/angdist.f
+++ src/angdist.f
@@ -5,11 +5,11 @@ c---- 67---- gamma angular distribution
       REAL pi
       parameter (pi = 3.14159265358979)
 
-c$$$c$$$  A uniform angular distribution for gammas
-c$$$      angdist = 1
-c$$$  A dipole angular distribution for gammas
-      angdist = (3./(8.*pi))*X**2
-c$$$c$$$  A quad. angular distribution for gammas
-c$$$      angdist = (15./(8.*pi))*(1.-X**2)*X**2
+C  A uniform angular distribution for gammas
+      angdist = 1
+CC  A dipole angular distribution for gammas
+C      angdist = (3./(8.*pi))*(1.-X**2)
+CC  A quad. angular distribution for gammas
+C      angdist = (15./(8.*pi))*(1.-X**2)*X**2
 
       END
